e2be5583187fe472dc4ddee1dbc7b495bbd7e60e
NexYY <NexYY@users.noreply.github.com>
2016-06-18 23:00:12 +0200

    update

--- src/setup_tensorflow_Python3.5_CPU.sh
+++ src/setup_tensorflow_Python3.5_CPU.sh
@@ -1,8 +1,12 @@
 #Install pip and Virtualenv:
 sudo apt-get install python-pip python-dev python-virtualenv python3-pip
 sudo pip install --upgrade pip
+sudo pip3 install --upgrade pip
 #Create a Virtualenv environment in the directory ~/tensorflow:
 sudo virtualenv --system-site-packages ~/virtualenv/tensorflow
+
+#Install Prettytensor
+sudo pip install prettytensor
 #Activate the environment:
 source ~/virtualenv/tensorflow/bin/activate
 
