097d403b60ee944966702a3aabf75042bd208c75
NexYY <NexYY@users.noreply.github.com>
2016-06-18 15:36:13 +0200

    added pip3

--- src/setup_tensorflow_Python3.5_CPU.sh
+++ src/setup_tensorflow_Python3.5_CPU.sh
@@ -1,9 +1,9 @@
 #Install pip and Virtualenv:
-sudo apt-get install python-pip python-dev python-virtualenv
+sudo apt-get install python-pip python-dev python-virtualenv python3-pip
 #Create a Virtualenv environment in the directory ~/tensorflow:
-virtualenv --system-site-packages ~/tensorflow
+virtualenv --system-site-packages ~/virtualenv/tensorflow
 #Activate the environment:
-source ~/tensorflow/bin/activate
+source ~/virtualenv/tensorflow/bin/activate
 
 #Ubuntu/Linux 64-bit, CPU only, Python 3.5
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0rc0-cp35-cp35m-linux_x86_64.whl
